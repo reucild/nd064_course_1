c61048303b46d2ffd4437664e6b41dff285e078f
reucild <171314979+reucild@users.noreply.github.com>
2024-05-31 15:42:05 +0900

    updated app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,20 @@
+import logging
+
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+DB_ACCESS_NUM = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    # count access to the database
+    global DB_ACCESS_NUM
+    DB_ACCESS_NUM += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +43,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info("Non-existing article was accessed, return 404 page")
       return render_template('404.html'), 404
     else:
+      title = post["title"]
+      app.logger.info(f"Article {title} retrieved")
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("About us page is retrieved")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,10 +72,52 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.info(f"New article {title} was created")
+
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# Define health check endpoint
+@app.route("/healthz")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+    )
+
+    return response
+
+# get total number of posts
+def _get_post_num():
+    # connection = sqlite3.connect("database.db")
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("SELECT title FROM posts")
+    title_list = cur.fetchall()
+    return len(title_list)
+
+# Define metrics endpoint
+@app.route('/metrics')
+def metrics():
+    post_count = _get_post_num()
+    response = app.response_class(
+        response=json.dumps(
+            {
+                "db_connection_count":DB_ACCESS_NUM,
+                "post_count":post_count,
+            }
+        ),
+        status=200,
+    )
+
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(
+        format="%(levelname)s: %(asctime)s %(message)s",
+        level=logging.DEBUG,
+        datefmt='%d/%m/%Y %H:%M:%S',
+        )
+    app.run(host='0.0.0.0', port='3111')
